60f6c25dd7ce0fc459e29459496892021c3e7a43
Luis Rodrigues <luis.cmendes.rodrigues@gmail.com>
2019-05-25 04:23:57 +0100

    falta converter o json pra um cryptoKey obj

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
 console.log("I'm alive");
 
-const crypto = require('crypto')
+const { Crypto } = require("@peculiar/webcrypto");
+const crypto = new Crypto();
+
 const express = require('express')
-const path = require('path');
-const app = express()
+const path = require('path')
+
+var util= require('util');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 const port = 3000
 
-app.use(express.static("public")) // define a pasta "root" onde são procurados ficheiros estáticos ex scripts de javascript
+var client_publicKey
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}!`)
-})
+app.use(express.static(__dirname + "/public"))  // define a pasta "root" onde são procurados ficheiros estáticos ex scripts de javascript
+
+io.on('connection',function(socket){
+	console.log("User connected")
+
+	socket.on("send_public_key", (data) =>{
+		// data = {
+		// 	"alg": "RSA-OAEP-256",
+		// 	"e": "AQAB",
+		// 	"ext": true,
+		// 	"key_ops": [
+		// 	"encrypt"
+		// 	],
+		// 	"kty": "RSA",
+		// 	"n": "qpQq_P-hJqtqpQPSGduc-xcqcRRzt5tOGrvOO-M5vL5zcWA1NBOCYx9NXhFmLBTMOg1o-36YtmW36QFP-PGP3uM8A53pyZMzl-NAE_CYUE85g1Xj3VamZN8hKX4Lp1_MN8edYXekWllsQxAhdfRrwuNX108r59SVtqoq9wVU9ts"
+		// }
+
+		json_to_key(data).then(key => {
+			print("something happened");
+			print("Server received: " + key_print(key));
+			
+			// var ct = encrypt("hi", key).then(
+			// print(ct)
+		})
+		
+		
+	});
+
+	socket.on('disconnect',function(){
+		console.log('user disconnected')
+	}
+)})
+
+
+http.listen(port, function(){
+	console.log('listening on *:3000');
+  });
 
 app.get('/',(req,res)=>{
 	res.render("homepage.ejs")
@@ -20,13 +61,59 @@ app.get('/register', (req,res)=>{
 	res.render("register.ejs")
 })
 
-app.get('/register/:pk/:user', (req,res) =>{
-	console.log("User:"+req.params.user)
-	console.log("Public Key:"+req.params.pk)
-	res.render("working.ejs")
-	//we save the fantastic key
-})
+// app.get('/register/:pk/:user', (req,res) =>{
+// 	console.log("User:"+req.params.user)
+// 	console.log("Public Key:"+req.params.pk)
+// 	// res.render("working.ejs")
+// 	//we save the fantastic key
+// })
 
 /*app.get('*', (req, res) => {
 	res.redirect("404 error")
 })*/
+
+function encrypt(text, publicKey){
+	text = "ola ;)"
+	let enc = new util.TextEncoder();
+	encoded_text = enc.encode(text);
+	
+	let ct = crypto.subtle.encrypt({name: "RSA-OAEP"}, publicKey, encoded_text );
+	print("ciphertext:" + ct)
+
+	return ct
+}
+
+function decrypt(ct, privateKey){
+	let enc = new util.TextEncoder();
+	let encoded_ct = enc.encode(ct)
+	let dec = crypto.subtle.decrypt({name:"RSA-OAEP"}, privateKey, encoded_ct);
+
+	print("123" + dec)
+
+ return dec
+}
+
+function print(stuff){
+	console.log(stuff)
+ }
+
+ /**
+ * retorna uma string com os valores da chave recebida
+ * @param {*} key chave a imprimir
+ * @returns string da chave
+ */
+function key_print(key){
+	return JSON.stringify(key, null, " ")
+ }
+
+ /**
+  * retorna o objecto do tipo crypto key
+  * @param {*} key_data normalmente um json que corresponde aos parametros da chave
+  */
+ function json_to_key(key_data){
+	return crypto.subtle.importKey("jwk", key_data,{ name: "RSA-OAEP", hash: "SHA-256" },
+									true, ["encrypt"] ).then( function (result){
+										client_publicKey = result
+										print("~~~~~ key returning: ~~~~~\n " + key_print(result))
+									});
+ }
--- public/registry.js
+++ public/registry.js
@@ -1,4 +1,5 @@
-const fs= require('fs');
+// const fs= require('fs');
+
 function arrayBufferToString(buffer)
 {
     var str = "";
@@ -14,10 +15,10 @@ function register() {
    a.innerHTML = "Clicked!"// diz o html a ser colocado dentro desse elento
    document.body.appendChild(a);   // faz append do elemento à página
    utilizador= document.getElementById("User").value
-
+   
    window.crypto.subtle.generateKey({
       name: "RSA-OAEP",
-      modulusLength: 4096,
+      modulusLength: 1024,
       publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
       hash: 'SHA-256', 
       },
@@ -25,27 +26,61 @@ function register() {
       ["encrypt", "decrypt"]
 
    ).then((key) => {
+      //check JWK parameters here https://www.iana.org/assignments/jose/jose.xhtml ou no rfc7518
+      //extrair a chave privada e meter no local storage
+      sk = window.crypto.subtle.exportKey("jwk",key.privateKey).then ((sk)=>{
+         print("client secret key - " + key_print(sk))
+         // print("client secret key d (Private Exponent) value: " +  sk.d)
 
-      sk= window.crypto.subtle.exportKey("jwk",key.privateKey
+         localStorage.setItem('sk', key_print(sk));
 
-       ).then ((sk)=>{
-        
-         localStorage.setItem('sk', sk.n);
-
-       })
-
-         return window.crypto.subtle.exportKey(
-            "jwk",
-           key.publicKey
-         )
-            
+      })
+         //exporta a chave publica para ser processada
+         return window.crypto.subtle.exportKey("jwk",key.publicKey)
       }
-        
+   //a chave publica é enviada para o servidor
    ).then((exportedKey) => {
-      window.location.replace(window.location + "/" + exportedKey.n + "/" + utilizador)
+      print("client public key: " + key_print(exportedKey))
+      // window.location.replace(window.location + "/" + exportedKey.n + "/" + utilizador)
+      socket.emit("send_public_key", exportedKey)
+      
+      // print("storage test: " + key_print(localStorage.getItem("sk")))
+
      
     })
     
-
-    
 };
+
+
+function encrypt(text, publicKey){
+      text = "ola"
+      let enc = new TextEncoder();
+      encoded_data = enc.encode(data);
+      
+      let ct = window.crypto.subtle.encrypt({name: "RSA-OAEP"}, publicKey, encoded_data );
+      print("123" + ct)
+
+      return ct
+}
+
+function decrypt(ct, privateKey){
+   let encoded_ct = enc.encode(ct)
+   let dec = window.crypto.subtle.decrypt({name:"RSA-OAEP"}, privateKey, encoded_ct);
+   print("123" + dec)
+
+   return dec
+}
+
+
+function print(stuff){
+   console.log(stuff)
+}
+
+/**
+ * retorna uma string com os valores da chave recebida
+ * @param {*} key chave a imprimir
+ * @returns string da chave
+ */
+function key_print(key){
+   return JSON.stringify(key, null, " ")
+}
