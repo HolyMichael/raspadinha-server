15242191ed0145e124549eda47b404cd6fdbd1ec
João Freire <32776046+Galactic21@users.noreply.github.com>
2019-05-29 19:38:36 +0100

    jasdas

--- public/raspadinhasCliente.js
+++ public/raspadinhasCliente.js
@@ -2,8 +2,16 @@ function getRandom(){
   
   raspClient(Math.random());
 }
+function submitPoll(id){
+ 
+    document.getElementById(id).disabled = true;
+    setTimeout(function(){document.getElementById(id).disabled = false;},5000);
+
+
+}
 
 function raspClient(c) {
+
   if (c >= 0.5) {
     c = 1;
   } else {
@@ -11,5 +19,19 @@ function raspClient(c) {
   }
   socket.emit("raspServer", c);
   console.log('O valor de c antes da normalização é: ', c);
+  socket.on("chave", function (data) {
+	
+    a = document.createElement("p"); // Cria um novo elemento do tipo p
+    a.innerHTML = data; // diz o html a ser colocado dentro desse elento
+    document.body.appendChild(a);   // faz append do elemento à página
+  });
+  
+  socket.on("Resultado", function (data) {
+	
+    a = document.createElement("p"); // Cria um novo elemento do tipo p
+    a.innerHTML = data; // diz o html a ser colocado dentro desse elento
+    document.body.appendChild(a);   // faz append do elemento à página
+  });
+  
 }
 
